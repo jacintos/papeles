206ae9c0dc86f5370cc7bd23630290aeee12c362
Jacinto Shy, Jr <jacinto.m.shy@gmail.com>
2009-03-21 08:59:25 -0400

    Some code cleanup.

--- src/PdfDocument.cs
+++ src/PdfDocument.cs
@@ -18,117 +18,105 @@
  */
 
 using Poppler;
+using System;
 
 namespace Papeles
 {
-  public class PdfDocument : IDocument
-  {
-    Document document;
-    DocumentInfo docInfo;
-    string password;
+	public class PdfDocument : IDocument
+	{
+		Document document;
+		DocumentInfo docInfo;
 
-    public int NPages
-    {
-      get {
-        return document.NPages;
-      }
-    }
+		public int NPages {
+			get { return document.NPages; }
+		}
 
-    public DocumentInfo Info
-    {
-      get {
-        if (docInfo == null) {
-          docInfo = new DocumentInfo();
-          docInfo.Title = document.Title;
-          docInfo.Format = document.Format;
-          docInfo.Author = document.Author;
-          docInfo.Subject = document.Subject;
-          docInfo.Keywords = document.Keywords;
-          docInfo.Creator = document.Creator;
-          docInfo.Producer = document.Producer;
-          docInfo.CreationDate = document.CreationDate;
-          docInfo.ModificationDate = document.ModificationDate;
-          docInfo.Linearized = document.Linearized;
-          docInfo.Metadata = document.Metadata;
-        }
-        return docInfo;
-      }
-    }
+		public DocumentInfo Info {
+			get {
+				if (docInfo == null) {
+					docInfo = new DocumentInfo ();
+					docInfo.Title = document.Title;
+					docInfo.Format = document.Format;
+					docInfo.Author = document.Author;
+					docInfo.Subject = document.Subject;
+					docInfo.Keywords = document.Keywords;
+					docInfo.Creator = document.Creator;
+					docInfo.Producer = document.Producer;
+					docInfo.CreationDate = document.CreationDate;
+					docInfo.ModificationDate = document.ModificationDate;
+					docInfo.Linearized = document.Linearized;
+					docInfo.Metadata = document.Metadata;
+				}
+				return docInfo;
+			}
+		}
 
-    public string Password
-    {
-      get {
-        return password;
-      }
-      set {
-        password = value;
-      }
-    }
+		public string Password { get; set; }
 
-    public PdfDocument()
-    {
-      password = "";
-    }
+		public PdfDocument ()
+		{
+			Password = "";
+		}
 
-    public PdfDocument(string uri, string password)
-    {
-      this.password = password;
-      this.Load(uri);
-    }
+		public PdfDocument (string uri, string password)
+		{
+			Password = password;
+			Load (uri);
+		}
 
-    public void Load(string uri)
-    {
-      document = Document.NewFromFile(uri, password);
-    }
+		public void Load (string uri)
+		{
+			document = Document.NewFromFile (uri, Password);
+		}
 
-    // public Page GetPage(int index)
-    // {
-    //   if (document != null && index < document.NPages)
-    //     return document.GetPage(index);
-    //   else
-    //     return null;
-    // }
+		// public Page GetPage(int index)
+		// {
+		//   if (document != null && index < document.NPages)
+		//     return document.GetPage(index);
+		//   else
+		//     return null;
+		// }
 
-    public void GetPageSize(int pageIndex, out int width, out int height)
-    {
-      Page page;
-      double pageWidth, pageHeight;
+		public void GetPageSize (int pageIndex, out int width, out int height)
+		{
+			Page page;
+			double pageWidth, pageHeight;
 
-      if (document != null && pageIndex < document.NPages) {
-        page = document.GetPage(pageIndex);
-        page.GetSize(out pageWidth, out pageHeight);
-        width = (int)pageWidth;
-        height = (int)pageHeight;
-      } else {
-        height = width = 0; // FIXME: throw an exception
-      }
-    }
+			if (document != null && pageIndex < document.NPages) {
+				page = document.GetPage (pageIndex);
+				page.GetSize (out pageWidth, out pageHeight);
+				width = (int) pageWidth;
+				height = (int) pageHeight;
+			} else {
+				height = width = 0; // FIXME: throw an exception
+			}
+		}
 
-    public void Render(RenderContext rc, Gdk.Drawable drawable)
-    {
-      Page page;
-      Cairo.Context context;
-      double width, height;
+		public void Render (RenderContext rc, Gdk.Drawable drawable)
+		{
+			Page page;
+			Cairo.Context context;
+			double width, height;
 
-      if (document != null && rc.pageIndex < document.NPages) {
-        page = document.GetPage(rc.pageIndex);
-      } else {
-        System.Console.WriteLine("Bad page index");
-        return;
-      }
+			if (document != null && rc.pageIndex < document.NPages) {
+				page = document.GetPage (rc.pageIndex);
+			} else {
+				Console.WriteLine ("Bad page index");
+				return;
+			}
 
-      page.GetSize(out width, out height);
-      width *= rc.scale;
-      height *= rc.scale;
+			page.GetSize (out width, out height);
+			width *= rc.scale;
+			height *= rc.scale;
 
-      context = Gdk.CairoHelper.Create(drawable);
-      // context.Rectangle(0.0, 0.0, width, height);
-      // context.Clip();
-      // context.Scale(rc.scale, rc.scale);
-      page.Render(context);
+			context = Gdk.CairoHelper.Create (drawable);
+			// context.Rectangle(0.0, 0.0, width, height);
+			// context.Clip();
+			// context.Scale(rc.scale, rc.scale);
+			page.Render (context);
 
-      // Garbage collection not currently supported in Mono.Cairo
-      ((System.IDisposable)context).Dispose();
-    }
-  }
+			// Garbage collection not currently supported in Mono.Cairo
+			(context as System.IDisposable).Dispose ();
+		}
+	}
 }
--- src/RenderContext.cs
+++ src/RenderContext.cs
@@ -19,17 +19,17 @@
 
 namespace Papeles
 {
-  public struct RenderContext
-  {
-    public int pageIndex;
-    public int rotation;
-    public double scale;
+	public struct RenderContext
+	{
+		public int pageIndex;
+		public int rotation;
+		public double scale;
 
-    public RenderContext(int pageIndex, int rotation, double scale)
-    {
-      this.pageIndex = pageIndex;
-      this.rotation = rotation;
-      this.scale = scale;
-    }
-  }
+		public RenderContext (int pageIndex, int rotation, double scale)
+		{
+			this.pageIndex = pageIndex;
+			this.rotation = rotation;
+			this.scale = scale;
+		}
+	}
 }
